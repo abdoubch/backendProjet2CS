8b606c39f5e043aa64f68c009a8abe3b3a2aebd3
abdoubch <la_bouchendouka@esi.dz>
2025-06-15 00:08:11 +0200

    ajout probelme c bon

--- src/main/java/com/example/demo/model/Problemes.java
+++ src/main/java/com/example/demo/model/Problemes.java
@@ -1,10 +1,12 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import java.util.List;
 
 @Entity
-@Table(name = "problemes",schema = "SYSTEM")
+@Table(name = "problemes", schema = "SYSTEM")
 public class Problemes {
 
     @Id
@@ -26,14 +28,20 @@ public class Problemes {
     @Column(name = "solution")
     private List<String> solutions;
 
+    @Column(nullable = false)
+    @Min(1)
+    @Max(3)
+    private int gravite; // Gravité du problème, de 1 à 3
+
     // Constructors
     public Problemes() {}
 
-    public Problemes(String name, String type, DailyReport dailyReport, List<String> solutions) {
+    public Problemes(String name, String type, DailyReport dailyReport, List<String> solutions, int gravite) {
         this.name = name;
         this.type = type;
         this.dailyReport = dailyReport;
         this.solutions = solutions;
+        this.gravite = gravite;
     }
 
     // Getters and Setters
@@ -51,4 +59,7 @@ public class Problemes {
 
     public List<String> getSolutions() { return solutions; }
     public void setSolutions(List<String> solutions) { this.solutions = solutions; }
+
+    public int getGravite() { return gravite; }
+    public void setGravite(int gravite) { this.gravite = gravite; }
 }
